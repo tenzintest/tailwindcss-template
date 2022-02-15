2d8069c185a8ca08b8538778f4a1bd15de8710ab
Tenzin Sean <tenzin.jeunesse@gmail.com>
2022-02-15 23:33:04 +0100

    first template

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  purge: [],
+  darkMode: false,
+  content: [],
+  theme: {
+    extend: {
+      colors: {
+        "pro-blue": "#242A45",
+        "pro-one": "#FE9636",
+      }
+    },
+  },
+  plugins: [],
+}
